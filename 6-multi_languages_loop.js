0a1504dedf87d3f9087bc583bddfb02964bf1682
Yaw Oduro <donkoryaw01@gmail.com>
2025-09-14 22:51:36 +0000

    Add script that contains a loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// Create an array with the 3 strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through the array
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
